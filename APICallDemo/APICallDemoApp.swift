3d24754983b665adc309f56c52e2b5553662d8ce
Harry-Kwesi <harrydegraft@hotmail.com>
2024-01-16 08:03:17 +0530

    Initial Commit

---
+++ APICallDemo/APICallDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallDemoApp.swift
+//  APICallDemo
+//
+//  Created by Harry Kwesi De Graft on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct APICallDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
